6beed42e8fd2283c008731870f68c60bed5d813d
GreyWolf <szymonstrozynskiii@gmail.com>
2021-03-09 15:12:12 +0100

    Added database

--- flaskblog.py
+++ flaskblog.py
@@ -1,9 +1,38 @@
+from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = '987aaf573ec33656fdad44edd26639ee6b34e34cea609e6336deec30044385a5'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite3:///site.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False,
+                           default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False,
+                            default="datetime.utcnow")
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+
 
 posts = [
     {
@@ -21,18 +50,18 @@ posts = [
 ]
 
 
-@app.route('/')
-@app.route('/home')
+@ app.route('/')
+@ app.route('/home')
 def home():
     return render_template('home.html', posts=posts)
 
 
-@app.route('/about')
+@ app.route('/about')
 def about():
     return render_template('about.html', title="About")
 
 
-@app.route('/register', methods=['GET', 'POST'])
+@ app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
@@ -41,7 +70,7 @@ def register():
     return render_template('register.html', title="Register", form=form)
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@ app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
