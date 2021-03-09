3b9de0849affe04c6a6d7d366ecb1340ac05ac57
GreyWolf <szymonstrozynskiii@gmail.com>
2021-03-09 16:26:26 +0100

    Added database file

--- flaskblog.py
+++ flaskblog.py
@@ -5,7 +5,7 @@ from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '987aaf573ec33656fdad44edd26639ee6b34e34cea609e6336deec30044385a5'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite3:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
 
